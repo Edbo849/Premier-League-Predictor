52654b38267e34355c69be8d338983f933e3ab39
Edbo849 <edborthwick84@gmail.com>
2025-09-15 09:52:09 +0100

    Imoroved precision by adding rolling averages

--- premier_league_predictor/predictor.py
+++ premier_league_predictor/predictor.py
@@ -26,7 +26,7 @@ def clean_data(data: pd.DataFrame) -> pd.DataFrame:
     data["venue_code"] = data["venue"].astype("category").cat.codes
 
     # Creates a code for each opposition
-    data["opp_code"] = data["opposition"].astype("category").cat.codes
+    data["opp_code"] = data["opponent"].astype("category").cat.codes
 
     # Converts time into just hour e.g 16:30 -> 16
     data["hour"] = data["times"].str.replace(":.+", "", regex=True).astype("int")
@@ -35,6 +35,30 @@ def clean_data(data: pd.DataFrame) -> pd.DataFrame:
     data["day_code"] = data["date"].dt.day_of_week
 
     return data
+
+
+def rolling_averages(group: pd.DataFrame, cols: list, new_cols: list) -> pd.DataFrame:
+    """Calculates rolling averages for specified columns.
+    Args:
+        group (pd.DataFrame): Grouped match data for a team.
+        cols (list): List of columns to calculate rolling averages for.
+        new_cols (list): List of new column names for the rolling averages.
+    Returns:
+        pd.DataFrame: DataFrame with rolling averages added."""
+
+    # Sort by date
+    group = group.sort_values("date")
+
+    # Calculate rolling averages
+    rolling_stats = group[cols].rolling(3, closed="left").mean()
+
+    # Rename columns
+    group[new_cols] = rolling_stats
+
+    # Drop rows with NaN values in new columns
+    group = group.dropna(subset=new_cols)
+
+    return group
 
 
 def train_model(matches: pd.DataFrame) -> None:
@@ -70,8 +94,23 @@ def main():
     # Set target for ML model
     matches["target"] = (matches["result"] == "W").astype("int")
 
+    # Define group for rolling averages
+    group_matches = matches.groupby("team")
+
+    group = group_matches.get_group("Manchester City").sort_values("date")
+
+    # Define columns for rolling averages
+    cols = ["gf", "ga", "sh", "sot", "dist", "fk", "pk", "pkatt"]
+    new_cols = [f"{c}_rolling" for c in cols]
+
+    matches_rolling = matches.groupby("team").apply(
+        lambda x: rolling_averages(x, cols, new_cols)
+    )
+    matches_rolling = matches_rolling.droplevel("team")
+    matches_rolling.index = pd.RangeIndex(start=0, stop=matches_rolling.shape[0])
+
     # Train the model
-    train_model(matches)
+    train_model(matches_rolling)
 
 
 if __name__ == "__main__":
